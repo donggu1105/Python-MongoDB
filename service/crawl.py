a87852be6cf28ad761f14f868b64a26f2911fb1c
donggu1105 <kbh11051@gmail.com>
2021-03-23 17:31:47 +0900

    data set

--- service/crawl.py
+++ service/crawl.py
@@ -15,67 +15,81 @@ actor_collection = actor_db.actor_collection
 
 
 
-url = "http://www.cine21.com/rank/person/content"
-post_data = dict()
-post_data["section"]  = "actor"
-post_data["period_start"] = "2021-02"
-post_data["gender"] = "all"
-actors_detail_info = list()
 
-for index in range(1,21):
-     post_data["page"] = index
+#
+# url = "http://www.cine21.com/rank/person/content"
+# post_data = dict()
+# post_data["section"]  = "actor"
+# post_data["period_start"] = "2021-02"
+# post_data["gender"] = "all"
+# actors_detail_info = list()
+#
+# for index in range(1,21):
+#      post_data["page"] = index
+#
+#      res = requests.post(url, data=post_data)
+#
+#      soup = BeautifulSoup(res.content, "html.parser")
+#      actors = soup.select("li.people_li div.name")
+#      hits = soup.select("ul.num_info > li > strong")
+#      movie_list = soup.select('ul.mov_list')
+#      ranks = soup.select('li.people_li span.grade')
+#
+#
+#
+#      for index, actor in enumerate(actors):
+#           # print(re.sub('\(\w*\)', '', actor.text))
+#           actor_name = re.sub('\(\w*\)','', actor.text)
+#           actor_hits = int(hits[index].text.replace(',',''))
+#           rank = int(ranks[index].text)
+#           movie_titles = movie_list[index].select('li span')
+#           movie_title_list = list()
+#           for movie_title in movie_titles:
+#                movie_title_list.append(movie_title.text)
+#
+#
+#
+#           actor_link = 'http://www.cine21.com'+actor.select_one('a').attrs['href']
+#           response_actor = requests.get(actor_link)
+#           soup_actor = BeautifulSoup(response_actor.content, "html.parser")
+#           default_info = soup_actor.select_one("ul.default_info")
+#           actor_details = default_info.select("li")
+#
+#
+#           actor_info_dict = dict()
+#
+#           actor_info_dict["배우이름"] = actor_name
+#           actor_info_dict["흥행지수"] = actor_hits
+#           actor_info_dict["출연영화"] = movie_title_list
+#           actor_info_dict["랭킹"] = rank
+#
+#           for actor_item in actor_details:
+#                actor_item_field = actor_item.select_one('span.tit').text
+#                actor_item_value = re.sub('<span.*?>.*?</span>', '', str(actor_item))
+#                actor_item_value = re.sub('<.*?>','', actor_item_value)
+#
+#                actor_info_dict[actor_item_field] = actor_item_value
+#
+#           actors_detail_info.append(actor_info_dict)
+#
+#
+#
+# pprint(actors_detail_info)
+#
+# actor_collection.insert_many(actors_detail_info)
 
-     res = requests.post(url, data=post_data)
-
-     soup = BeautifulSoup(res.content, "html.parser")
-     actors = soup.select("li.people_li div.name")
-     hits = soup.select("ul.num_info > li > strong")
-     movie_list = soup.select('ul.mov_list')
-     ranks = soup.select('li.people_li span.grade')
-
-
-
-     for index, actor in enumerate(actors):
-          # print(re.sub('\(\w*\)', '', actor.text))
-          actor_name = re.sub('\(\w*\)','', actor.text)
-          actor_hits = int(hits[index].text.replace(',',''))
-          rank = int(ranks[index].text)
-          movie_titles = movie_list[index].select('li span')
-          movie_title_list = list()
-          for movie_title in movie_titles:
-               movie_title_list.append(movie_title.text)
-
-
-
-          actor_link = 'http://www.cine21.com'+actor.select_one('a').attrs['href']
-          response_actor = requests.get(actor_link)
-          soup_actor = BeautifulSoup(response_actor.content, "html.parser")
-          default_info = soup_actor.select_one("ul.default_info")
-          actor_details = default_info.select("li")
-
-
-          actor_info_dict = dict()
-
-          actor_info_dict["배우이름"] = actor_name
-          actor_info_dict["흥행지수"] = actor_hits
-          actor_info_dict["출연영화"] = movie_title_list
-          actor_info_dict["랭킹"] = rank
-
-          for actor_item in actor_details:
-               actor_item_field = actor_item.select_one('span.tit').text
-               actor_item_value = re.sub('<span.*?>.*?</span>', '', str(actor_item))
-               actor_item_value = re.sub('<.*?>','', actor_item_value)
-
-               actor_info_dict[actor_item_field] = actor_item_value
-
-          actors_detail_info.append(actor_info_dict)
-
-
-
-pprint(actors_detail_info)
+# data_list = actor_collection.find({'출연영화' : {"$size" : 2}})
+#
+#
+# for data in data_list:
+#     print(data)
 
 # 특수한 정규 표현식
 
 # Greedy vs. NonGreedy
 # . 문자ㅡㄴ 줄바꿈 누자인 \n 을 제외한 모든 문자 한개를 의미함
 #  * 는 앞 문자가 0번또는 그 이상 반복되는 패턴
+
+data_list = actor_collection.find({"출연영화" : {"$elemMatch" : ["국가부도의 날"]}}).sort("흥행지수", pymongo.DESCENDING).limit(10)
+for data in data_list:
+     print(data)
